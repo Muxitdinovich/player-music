3d8e1154665c947197a6211e251abbd69308f783
the-shamshi <135255247+Muxitdinovich@users.noreply.github.com>
2023-10-23 23:47:22 +0500

    player music

---
+++ js/music-list.js
@@ -0,0 +1,124 @@
+let allMusic = [
+  {
+    name: "MEMORY REBOOT",
+    artist: "Memory Reboot",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "King Baldwin IV x Memory Reboot (Slowed) ",
+    artist: "Memory Reboot (Slowed) ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Mareux - The Perfect Girl",
+    artist: "Ramona Flowers",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Barbie × Oppenheimer",
+    artist: "Barbie × Oppenheimer",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "SASHA-CYBERPUNK-EDGERUNNERS",
+    artist: "METAMORPHOSIS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "The Perfect Girl - Mareux",
+    artist: "Sigma",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "L O N E L I N E S S",
+    artist: "Sigma | Snowfall",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "LITERALLY ME",
+    artist: "Sigma",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "PASTEL GHOST - SILHOUETTE (slowed + reverb)",
+    artist: "Sigma",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Mr.Kitty",
+    artist: "𝐌𝐫. 𝐥𝐚𝐦𝐛𝐞𝐫𝐭",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Mr.Kitty • After Dark ",
+    artist: "Jennifer Connelly",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Sasha edit",
+    artist: "Metamorphis",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Devil Eyes",
+    artist: "Cyberpunk:Sasha Yakovleva",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Kaleo — Way Down We Go",
+    artist: "Kaleo — Way Down We Go",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "MoonDeity - WAKE UP",
+    artist: "Sigma",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "ROSA WALTON & HALLIE COGGINS",
+    artist: "CYBERPUNK",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "modern talking - cheri cheri lady",
+    artist: "cheri cheri lady",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Tessa Violet - Crush (Official Music Video)",
+    artist: "Tessa Violet",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "kletka(клетка)-molchat doma slowed & reverb",
+    artist: "kletka(клетка)",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Blade Runner 2049",
+    artist: "love",
+    img: "music-20",
+    src: "music-20"
+  },
+
+
+];
